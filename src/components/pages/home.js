80178d9ac4f4a7b146d84971d2b78a2879e7afaa
apoorva srivastava <apoorvasrivastava.14@gmail.com>
2020-10-14 20:46:56 +0530

    added services and portfolio

--- src/components/pages/home.js
+++ src/components/pages/home.js
@@ -1,18 +1,27 @@
 import React, {Component} from 'react';
 import Header from '../Common/Header'
 import image from '../../assets/img/header-bg.jpg'
+import Services from '../Common/services'
+import Portfolio from '../Common/portfolio'
+
+
 
 class Home extends Component{
     render(){
         return(
-           <Header 
+            <div>
+                <Header 
                 title = "The only blog you need."
                 subtitle = "ACTUALITY"
                 buttonText = "know more"
                 link = "/services"
                 showButton = {true}
                 image = {image}
-           />
+                 />
+                <Services />
+                <Portfolio />
+            </div>
+           
         );
     }
 }
